f15c3ba2be01c900703dd197e38994ee39847116
Pako2425 <patryk.kocinski340@gmail.com>
2024-06-07 05:47:43 +0200

    Init backend project

---
+++ backend/src/main/java/com/connectme/connectme/ConnectmeApplication.java
@@ -0,0 +1,13 @@
+package com.connectme.connectme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectmeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/connectme/connectme/ConnectmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectme.connectme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
