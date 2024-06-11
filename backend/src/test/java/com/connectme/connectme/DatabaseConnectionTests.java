8e82c364de8ce99552e145db6a0338817a11002a
Pako2425 <patryk.kocinski340@gmail.com>
2024-06-11 12:44:34 +0200

    Test connection with database. Success

--- backend/src/test/java/com/connectme/connectme/DatabaseConnectionTests.java
+++ backend/src/test/java/com/connectme/connectme/DatabaseConnectionTests.java
@@ -1,16 +1,25 @@
 package com.connectme.connectme;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
 
-@DataJpaTest
+@JdbcTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@TestPropertySource(locations = "classpath:application-test.properties")
 public class DatabaseConnectionTests {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Test
-    void connectWithDatabaseText() {
+    public void connectWithDatabaseTest() {
+        Integer result = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+        assertThat(result).isEqualTo(1);
     }
 }
