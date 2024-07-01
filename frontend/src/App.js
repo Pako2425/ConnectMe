4ef92b3825d5f90344407cd5289e5ff4046311a8
Pako2425 <patryk.kocinski340@gmail.com>
2024-07-01 16:39:37 +0200

    User Section added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import TopBar from './TopBar.js'
+import UserSection from './UserSection.js'
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <UserSection />
+      
+
     </div>
   );
 }
---
+++ frontend/src/UserSection.js
@@ -0,0 +1,19 @@
+import './UserSection.css'
+
+function UserSection() {
+    return(
+        <div className='user-section'>
+            <img 
+                className='user-section-image'
+                src=""
+                alt="User-Image">
+
+            </img>
+            <p className='user-section-text'>
+                User name
+            </p>
+        </div>
+    );
+}
+
+export default UserSection;
